00789b2e3ca3dcab29021f1cde515135f1c2887f
artyomdrobysh <artyom.drobysh96@gmail.com>
2021-01-20 22:43:09 +0300

    init

---
+++ src/main/java/by/itninjas/watchdogs/WatchDogsApplication.java
@@ -0,0 +1,16 @@
+package by.itninjas.watchdogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point to the application <b>Watch dogs</b>.
+ */
+@SpringBootApplication
+public class WatchDogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchDogsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/itninjas/watchdogs/WatchDogsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.itninjas.watchdogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchDogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
